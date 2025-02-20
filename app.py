641e226bb668aebc0ab73c083002be5fc53ae714
Toust <latostadorano@gmail.com>
2025-02-19 20:27:03 -0600

    chceckpoints not buttons

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import plotly.express as px
 import streamlit as st
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-st.header('Lanzar una moneda') # crear un header
-hist_button = st.button('Construir histograma') # crear un botón
-scatter_button = st.button('Construir gráfico disperción') #crear otro botón
+st.header('Crear gráficos con streamlit y plotly') # crear un header
+build_hist = st.checkbox('Construir histograma') # crear un botón
+build_scatter = st.checkbox('Construir gráfico dispersion') #crear otro botón
         
-if hist_button: # al hacer clic en el botón
+if build_hist: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
@@ -15,10 +15,10 @@ if hist_button: # al hacer clic en el botón
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
-if scatter_button: 
-    st.write('Creación de un gráfico de disperción para el conjunto de datos de anuncios de venta de coches')
+if build_scatter: 
+    st.write('Creación de un gráfico de dispersion para el conjunto de datos de anuncios de venta de coches')
 
-    # crear un gráfico de disperción
+    # crear un gráfico de dispersion
     fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
     st.plotly_chart(fig, use_container_width=True)
     
